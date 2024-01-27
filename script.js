899cd0ebc8f82693e72cb36ef83370594a552649
Ohjay33 <61249083+Ohjay33@users.noreply.github.com>
2024-01-27 17:18:49 -0600

    commit1

--- script.js
+++ script.js
@@ -1,45 +1,80 @@
-function colorchange() {
-    var action = document.querySelector("body")
-    action.style.backgroundColor = "red"
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
 
-}
+canvas.height = window.innerHeight - 80;
+canvas.width = window.innerWidth;
+
+let x = null,
+    y = null;
+context.lineWidth = 5;
 
 
-function setBg() {
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = "#" + randomColor;
-    console.log("Done! You did it!!!");
-    // color.innerHTML = "#" + randomColor;
-}
+let draw = false;
+context.strokeStyle = "#010600";
 
-function clrpickr() {
+window.addEventListener('mousedown', (e) => (draw = true));
+window.addEventListener('mouseup', (e) => (draw = false));
+
+
+window.addEventListener('mousemove', (e) => {
+    if (x == null || y == null || !draw) {
+        x = e.clientX;
+        y = e.clientY;
+        return;
+    }
+    let currentX = e.clientX,
+        currentY = e.clientY;
+
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(currentX, currentY);
+    context.stroke();
+
+    x = currentX;
+    y = currentY;
+
+});
+
+    // The function to clear the canvas
+    document.getElementById('clear').onclick = function() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    };
+
+    // function to pick the color and paint
+    document.querySelectorAll(".colorChange").forEach((el) => {
+        el.onclick = function () {
+            context.strokeStyle = el.style.backgroundColor;
+        };
+    });
+
+
 
     // Selection of id's and classes from html document 
-    const bgclr = document.getElementById("inpt");
-    const headingg = document.querySelector(".row");
+const bgclr = document.getElementById("clr"); 
+const headingg = document.querySelector(".head"); 
 
-    // Here we are adding event listener which 
-    // is used to detect the mouse movement 
-    bgclr.addEventListener("input", () => {
-        // This updates the background color which is 
-        // picked by the user from the picker 
-        document.body.style.backgroundColor = bgclr.value;
+// Here we are adding event listener which 
+// is used to detect the mouse movement 
+bgclr.addEventListener("input", () => { 
+// This updates the background color which is 
+// picked by the user from the picker 
+document.body.style.backgroundColor = bgclr.value; 
 
-        // This is the conditional statement that is used 
-        // to change the text color from BLACK to WHITE 
-        // when the background color changes to dark! 
-        if (
-            bgclr.value.includes("00") ||
-            bgclr.value.includes("0a") ||
-            bgclr.value.includes("0b") ||
-            bgclr.value.includes("0c") ||
-            bgclr.value.includes("0d") ||
-            bgclr.value.includes("0e") ||
-            bgclr.value.includes("0f")
-        ) {
-            headingg.style.color = "#fff";
-        } else {
-            headingg.style.color = "#000";
-        }
-    });
-}
+// This is the conditional statement that is used 
+// to change the text color from BLACK to WHITE 
+// when the background color changes to dark! 
+if ( 
+	bgclr.value.includes("00") || 
+	bgclr.value.includes("0a") || 
+	bgclr.value.includes("0b") || 
+	bgclr.value.includes("0c") || 
+	bgclr.value.includes("0d") || 
+	bgclr.value.includes("0e") || 
+	bgclr.value.includes("0f") 
+) { 
+	headingg.style.color = "#fff"; 
+} else { 
+	headingg.style.color = "#000"; 
+} 
+}); 
+
